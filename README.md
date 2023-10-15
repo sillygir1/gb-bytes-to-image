732ae3c41e43186be7227e7df1c38f1fd51410b4
sillygir1 <25554397+sillygir1@users.noreply.github.com>
2023-10-15 10:35:00 +0400

    Change format of arguments.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 ## Convert gameboy/pokewalker byte arrays into images
+
+Usage:
+
+```c
+bytes_to_image filename [option] start_address arg2
+  option:
+    -n  Read n (hexadecimal) bytes starting address start_address
+    -r  Read range start_address to arg2
+    -h  Help
+```
--- bytes_to_image.c
+++ bytes_to_image.c
@@ -1,6 +1,8 @@
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // Gonna think about that one later, useless for now
 void swap(uint16_t *x, uint16_t *y) {
@@ -9,10 +11,10 @@ void swap(uint16_t *x, uint16_t *y) {
 	*y = temp;
 } // Works i suppose
 
-// Read *lenght* bytes to buffer from file starting from offset
-uint32_t read(FILE *file, uint8_t *buff, uint32_t lenght, uint32_t offset) {
-	fseek(file, offset, 0);
-	return fread(buff, 1, lenght, file);
+// Read *end* bytes to buffer from file starting from start
+uint32_t read(FILE *file, uint8_t *buff, uint32_t end, uint32_t start) {
+	fseek(file, start, 0);
+	return fread(buff, 1, end, file);
 } // Done
 
 // Sprites' pixels are stored in pairs of bytes, each two bytes
@@ -56,48 +58,75 @@ void draw_sprite(uint16_t *pixels, uint16_t size, uint8_t w, uint8_t h) {
 	}
 } // Done
 
+uint32_t start = 0;
+uint32_t end = 0;
+char *filename;
+
+void help() {
+	printf("usage: bytes_to_image filename [option] start_address arg2 \n");
+	printf("  option:\n");
+	printf("    -n\tRead n (hexadecimal) bytes starting address "
+	       "start_address\n");
+	printf("    -r\tRead range start_address to arg2\n");
+	printf("    -h\tHelp\n");
+}
+
+uint8_t parse_arguments(int argc, char *argv[]) {
+	if (argc > 1) {
+		start = (int)strtol(argv[3], NULL, 16);
+		if (strcmp(argv[1], "-h") == 0 ||
+		    strcmp(argv[2], "-h") == 0) { // Help
+			help();
+			return false;
+		} else {
+			filename = argv[1];
+		}
+		if (strcmp(argv[2], "-n") == 0) { // N bytes
+			end = (int)strtol(argv[4], NULL, 16);
+		} else if (strcmp(argv[2], "-r") == 0) { // Range
+			end = (int)strtol(argv[4], NULL, 16) - start + 1;
+		}
+	} else {
+		help();
+		return false;
+	}
+	return true;
+}
+
 int main(int argc, char *argv[]) {
+
+	filename = malloc(sizeof(char) * 64);
+
+	if (!parse_arguments(argc, argv)) {
+		return 1;
+	}
 	// Opening file
-	FILE *file = fopen("64k-full-rom.bin", "r");
+	FILE *file = fopen(filename, "r");
 
 	// Initializing variables and buffers
-	uint32_t lenght = 64;
-	uint32_t offset = 0x11D0;
-
-	// Arguments: sprites_to_pixels address length
-	// Arguments: sprites_to_pixels start_address end_address anything
-	// (needs 3 arguments for second one, i'll change it later)
-	if (argc > 1) {
-		offset = (int)strtol(argv[1], NULL, 16);
-		if (argc == 4) {
-			lenght = (int)strtol(argv[2], NULL, 16) - offset + 1;
-		} else
-			lenght = atoi(argv[2]);
-	}
-
-	uint8_t *buff = malloc(sizeof(uint8_t) * lenght);
-	uint16_t *buff_image = malloc(sizeof(uint16_t) * lenght);
+	uint8_t *buff = malloc(sizeof(uint8_t) * end);
+	uint16_t *buff_image = malloc(sizeof(uint16_t) * end);
 
 	// Reading from file
-	uint32_t bytes = read(file, buff, lenght, offset);
+	uint32_t bytes = read(file, buff, end, start);
 	if (bytes != 0) {
-		printf("Read %d bytes starting 0x%x:\n", bytes, offset);
+		printf("Read %d bytes starting 0x%x:\n", bytes, start);
 		for (uint32_t i = 0; i < bytes; i++) {
 			printf("0x%x, ", buff[i]);
 		}
 		printf("\n");
 	} else {
-		printf("Failure to read file on 0x%x offset\n", offset);
+		printf("Failure to read file on 0x%x offset\n", start);
 	}
 
 	printf("Joined bytes:\n");
-	for (uint32_t i = 0; i < lenght / 2; i++) {
+	for (uint32_t i = 0; i < end / 2; i++) {
 		buff_image[i] = join_bytes(buff[2 * i], buff[2 * i + 1]);
 		printf("0x%x, ", buff_image[i]);
 	}
 
 	printf("\nColor palette:\n ░▒▓\n");
-	draw_sprite(buff_image, lenght / 2, 8, 8);
+	draw_sprite(buff_image, end / 2, 8, 8);
 
 	free(buff);
 	fclose(file);
